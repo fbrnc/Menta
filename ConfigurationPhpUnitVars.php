2fbf8493a2860436c97f5fb1d3d0864d3f5ea552
Fabrizio Branca <mail@fabrizio-branca.de>
2011-11-29 21:31:57 +0100

    Changed phpdoc comment

--- ConfigurationPhpUnitVars.php
+++ ConfigurationPhpUnitVars.php
@@ -4,15 +4,6 @@
  * Configuration to get the config from xml files
  *
  * @author Fabrizio Branca
- * 
- * I did a rewrite of the class maintaining its api methods getValue() and issetKey().
- * Now it is possible to use the class completely static.
- * I've cleaned up the messy way how settings from phpunit's configuration file and
- * settings from the default.xml file were merged and overwritten into constants and
- * global variables. Now everything is handled via global variables same way like
- * phpunit internally handles settings set found in //phpunit/php/var.
- * Additionally now it is possible to have access settings set in the phpunit file, that
- * are not set in the default.xml (this was not possible before)
  */
 class Menta_ConfigurationPhpUnitVars implements Menta_Interface_Configuration {
 
